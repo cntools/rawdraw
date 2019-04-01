0314fc54a2888db834042ca67116eed4842c4f22
David Auer <dreua@posteo.de>
2019-04-01 02:06:56 +0200

    CNFGTackSegment in OpenGL: Draw GL_LINE_LOOP instead of GL_LINE
    Fix https://github.com/cnlohr/rawdraw/issues/13

--- CNFGFunctions.c
+++ CNFGFunctions.c
@@ -312,11 +312,14 @@ void CNFGTackSegment( short x1, short y1, short x2, short y2 )
 	{
 		glBegin( GL_POINTS );
 		glVertex2f( x1+.5, y1+.5 );
-		glEnd();		
+		glEnd();
 	}
 	else
 	{
-		glBegin( GL_LINES );
+		// GL_LINE misses the last pixel if the line is not continued
+		// due to the Diamond-exit rule so we use GL_LINE_LOOP which
+		// draws the line back and forth catching all the pixels.
+		glBegin( GL_LINE_LOOP );
 		glVertex2f( x1+.5, y1+.5 );
 		glVertex2f( x2+.5, y2+.5 );
 		glEnd();
