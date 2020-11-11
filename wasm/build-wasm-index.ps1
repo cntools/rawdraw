eb19f30989ec4ccfcf7a7bad709b5501629402d7
CNLohr <lohr85@gmail.com>
2020-11-11 14:36:46 -0800

    Fix build system in Windows to include uglification if possible.

--- wasm/build-wasm-index.ps1
+++ wasm/build-wasm-index.ps1
@@ -1,9 +1,13 @@
 # Install LLVM to C:\Program Files\LLVM or set parameter below (Recommends LLVM-11)
 # Install Binaryen to C:\binaryen\ or set parameter below (Recommends binaryen-98)
+# If you want uglify-js, install node to your system paths, https://nodejs.org/en/download/
+#  Execute: npm install -g terser
 
 $Clang = "C:\Program Files\LLVM\bin\clang"
-$WASMOpt = "C:\Users\caibi\Development\_Tools\binaryen-version_98\bin\wasm-opt"
-#$WASMOPT = "C:\binaryen\binaryen-version_98\bin\wasm-opt"
+
+# Assuming you have downloaded binaryen-98 and put it someheere that is in your paths.
+$WASMOpt = "wasm-opt"
+
 
 $OutFile = 'index.html'
 $TemplateHT = 'template.ht'
@@ -47,6 +51,18 @@ $ContentHT = Get-Content $TemplateHT -Raw
 $ContentJS = Get-Content $TemplateJS -Raw
 
 $Output = $ContentJS.Replace('${BLOB}', $WASMasB64);
+
+if (Get-Command "terser" -ErrorAction SilentlyContinue) 
+{
+    Set-Content main.js -Value $Output
+    terser main.js > main.min.js
+    $Output = Get-Content main.min.js -Raw
+}
+else
+{
+    Write-Host( "terser not found. Not uglifying javascript." );
+}
 $Output = $ContentHT.Replace('${JAVASCRIPT_DATA}', $Output);
 
 Set-Content $OutFile -Value $Output
+Write-Host ("$OutFile size: {0:N0} B" -f (Get-Item $OutFile).length)
