ee3033ea1ad85ef409139067b1f36e35cef8fd4a
cnlohr <lohr85@gmail.com>
2025-03-24 03:10:25 -0700

    Fix unterminated string.

--- wasm/subst.c
+++ wasm/subst.c
@@ -45,10 +45,11 @@ char *readfile(const char *filename, int flags)
 	size = ftell(file);
 	rewind(file);
 	
-	buffer = malloc(size);
+	buffer = malloc(size+1);
 
 	fread(buffer, size, 1, file);
-	
+	buffer[size] = 0;
+
 	if ((flags & SWALLOW) != 0) {
 		if (buffer[size - 1] == '\n')
 			buffer[size - 1] = 0x00;
